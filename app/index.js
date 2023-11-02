da24934233657195c1fc134c884644a18c6f4798
Filip <filip.kalicanin@quantox.com>
2023-11-02 17:03:51 +0100

    setting up the basics

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+
+export default function App() {
+  return (
+    <GluestackUIProvider config={config}>
+      <Box width='100%' justifyContent='center' alignItems='center'>
+        <Text>My first app</Text>
+      </Box>
+    </GluestackUIProvider>
+  );
+}
